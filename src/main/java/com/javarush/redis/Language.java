cdfed000c08b6947d73f1c04447182676812832b
shovk <shovkyntimofej@gmail.com>
2024-05-18 18:59:15 +0300

    adding all the code

---
+++ src/main/java/com/javarush/redis/Language.java
@@ -0,0 +1,17 @@
+package com.javarush.redis;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+
+@Setter
+@Getter
+public class Language {
+    private String language;
+    private Boolean isOfficial;
+    private BigDecimal percentage;
+
+}
+
